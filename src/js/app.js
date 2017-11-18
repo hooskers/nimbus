340afacc652708f0a16b356c315a5580fa9f2698
Wade Johnson <wadej93@gmail.com>
2017-11-18 10:34:59 -0800

    Initial setup

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>Wassup</div>
+        );
+    }
+}
+
+render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+const paths = {
+    DIST: path.resolve(__dirname, 'dist'),
+    SRC: path.resolve(__dirname, 'src'),
+    JS: path.resolve(__dirname, 'src/js'),
+}
+
+module.exports = {
+    entry: path.join(paths.JS, 'app.js'),
+    output: {
+        path: paths.DIST,
+        filename: 'app.bundle.js',
+    },
+    devServer: {
+        contentBase: paths.SRC,
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: path.join(paths.SRC, 'index.html'),
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader',
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+};
